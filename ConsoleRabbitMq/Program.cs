2bc2803e4fe8960cad1dbaf751570d9ea4cb45af
superfeeling <superfeeling@126.com>
2022-04-29 22:24:44 +0800

    '基本消息模式'

---
+++ ConsoleRabbitMq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ConsoleRabbitMq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queueName = nameof(Program.Main);
+
+            //durable:队列的持久化，autoDelete必须为false
+            channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+            //消息的持久化，否则RabbitMQ重启后只保留队列，不保留消息
+            var properties = channel.CreateBasicProperties();
+
+            properties.Persistent = true;
+
+            var input = string.Empty;
+
+            do
+            {
+                input = Console.ReadLine();
+
+                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: properties, body: Encoding.UTF8.GetBytes(input));
+
+            } while (input != "exit");
+        }
+    }
+}
---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsumerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queueName = nameof(Program.Main);
+
+            channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            //接收
+            consumer.Received += (sender, args) => {
+                var tag = args.DeliveryTag;
+                var key = args.RoutingKey;
+                var exchange = args.Exchange;
+                var body = args.Body.ToArray();
+                Console.WriteLine(Encoding.UTF8.GetString(body));
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            //消费
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
